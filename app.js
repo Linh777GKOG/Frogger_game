bd2d7ec3d721841b8cf4b9ed7559140343753db1
Linh Vu <linh777gkog@gmail.com>
2022-07-15 23:48:22 +0700

    commit

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ const logsLeft = document.querySelectorAll('.log-left');
 const logsRight = document.querySelectorAll('.log-right');
 const carsLeft = document.querySelectorAll('.car-left');
 const carsRight = document.querySelectorAll('.car-right');
+
+let currentIndex = 76;
+const width = 9;
+let timerId;
+let outcomeTimerId;
+let currentTime = 20;
