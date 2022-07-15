e822db473252059b15f6c3a899872b63af426d34
Linh Vu <linh777gkog@gmail.com>
2022-07-15 23:48:39 +0700

    ham move frog

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ const width = 9;
 let timerId;
 let outcomeTimerId;
 let currentTime = 20;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove('frog');
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case 'ArrowRight':
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case 'ArrowUp':
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case 'ArrowDown':
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add('frog');
+}
